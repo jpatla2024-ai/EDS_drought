f2f1fbac6e88641b707777c778a0154f1a1c7351
jpatla2024-ai <j_patla2024@coloradocollege.edu>
2025-10-28 15:51:12 -0600

    Update EV_Data_Story_Project.py

---
+++ EV_Data_Story_Project.py
@@ -0,0 +1,6 @@
+# Importing the necessary data packages to analyze our data.
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+
+# Will start to make a variable with the path to our data, once we've leanred more about working with gridded data.
