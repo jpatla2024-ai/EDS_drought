03bab2dc411f699d2d4c7e5fb597aae838a555f2
jpatla2024-ai <j_patla2024@coloradocollege.edu>
2025-10-30 09:44:00 -0600

    Update EV_Data_Story_Project.py

--- EV_Data_Story_Project.py
+++ EV_Data_Story_Project.py
@@ -2,5 +2,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+ds = xr.open_dataset()
 
 # Will start to make a variable with the path to our data, once we've leanred more about working with gridded data.
